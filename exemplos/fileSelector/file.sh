38ef2f763a0aceb365421beed6a9c00155eb3b6d
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-12-01 13:28:13 -0300

    Exemplo seletor de arquivos com o zenity

--- exemplos/fileSelector/file.sh
+++ exemplos/fileSelector/file.sh
@@ -1,10 +1,26 @@
-#!/bin/sh
+#!/bin/bash
+#
+# file.sh (Shell Script)
+# 
+# Objetivo: Exemplo seletor de arquivos no zenity.
+# 
+# Site: https://dirack.github.io
+# 
+# Versão 1.0
+# 
+# Programador: Rodolfo A C Neves (Dirack) 01/12/2020
+# 
+# Email: rodolfo_profissional@hotmail.com
+# 
+# Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
 
-FILE=`zenity --file-selection --title="Select a File"`
+FILE=$(zenity --file-selection --title="Select a File")
 
 case $? in
          0)
-                echo "\"$FILE\" selected.";;
+                echo "$FILE selected."
+		cat "$FILE"
+		;;
          1)
                 echo "No file selected.";;
         -1)
