bf2c219be2c8ed08432a2e79cfba72ffd3d42fae
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-12-01 12:12:44 -0300

    Exemplo seletor de cores

--- exemplos/color/color.sh
+++ exemplos/color/color.sh
@@ -1,6 +1,20 @@
-#!/bin/sh
+#!/bin/bash
+#
+# color.sh (Shell Script)
+# 
+# Objetivo: Exemplo de uso do seletor de cores do Zenity.
+# 
+# Site: https://dirack.github.io
+# 
+# Versão 1.0
+# 
+# Programador: Rodolfo A C Neves (Dirack) 01/12/2020
+# 
+# Email: rodolfo_profissional@hotmail.com
+# 
+# Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
 
-COLOR=$(zenity --color-selection --show-palette)
+COLOR=$(zenity --color-selection)
 
 case $? in
          0)
