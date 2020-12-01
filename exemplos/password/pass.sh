f9a700caf8c43d3bcc51358f832c676344c55a06
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-12-01 13:21:01 -0300

    Exemplo sistema de login com o Zenity

--- exemplos/password/pass.sh
+++ exemplos/password/pass.sh
@@ -1,11 +1,34 @@
-#!/bin/sh
+#!/bin/bash
+#
+# pass.sh (Shell Script)
+# 
+# Objetivo: Exemplo do leitor de senhas do zenity.
+# 
+# Site: https://dirack.github.io
+# 
+# Versão 1.0
+# 
+# Programador: Rodolfo A C Neves (Dirack) 01/12/2020
+# 
+# Email: rodolfo_profissional@hotmail.com
+# 
+# Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
 
-ENTRY=`zenity --password --username`
+ENTRY=$(zenity --password --username)
 
 case $? in
          0)
-	 	echo "User Name: `echo $ENTRY | cut -d'|' -f1`"
-	 	echo "Password : `echo $ENTRY | cut -d'|' -f2`"
+		USERNAME=$(echo "$ENTRY" | cut -d'|' -f1)
+		PASSWORD=$(echo "$ENTRY" | cut -d'|' -f2)
+	 	echo "User Name: $USERNAME"
+	 	echo "Password : $PASSWORD"
+
+		# Validação do login
+		if [ "$USERNAME" == "Rodolfo" -a "$PASSWORD" == "12345" ]
+		then
+			echo "Login"
+			echo "Hello, $USERNAME"
+		fi	
 		;;
          1)
                 echo "Stop login.";;
